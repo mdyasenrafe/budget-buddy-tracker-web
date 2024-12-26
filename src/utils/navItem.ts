820fc04882a1f32724ce19128ca88810aaf44b41
Md yasen rafe <mdyasenrafe@gmail.com>
2024-12-26 15:45:53 +0600

    updated nav items

--- src/utils/navItem.ts
+++ src/utils/navItem.ts
@@ -1,34 +1,38 @@
 import { NavItem } from "@/types";
-import { GrHomeRounded } from "react-icons/gr";
-import { MdAttachMoney, MdBarChart, MdCreditCard } from "react-icons/md";
+import {
+  AiOutlineHome,
+  AiOutlineFundProjectionScreen,
+  AiOutlineCreditCard,
+  AiOutlineDollarCircle,
+} from "react-icons/ai";
 
 export const navItems: NavItem[] = [
   {
     id: 1,
     label: "Home",
     path: "/dashboard",
-    icon: GrHomeRounded,
+    icon: AiOutlineHome,
     desktop: true,
   },
   {
     id: 2,
     label: "Analytics",
     path: "/analytics",
-    icon: MdBarChart,
+    icon: AiOutlineFundProjectionScreen,
     desktop: true,
   },
   {
     id: 3,
     label: "Cards",
     path: "/card-management",
-    icon: MdCreditCard,
+    icon: AiOutlineCreditCard,
     desktop: true,
   },
   {
     id: 4,
     label: "Budget",
     path: "/budget-tracker",
-    icon: MdAttachMoney,
+    icon: AiOutlineDollarCircle,
     desktop: true,
   },
 ];
