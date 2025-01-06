7d647ea9986e739ae2784b97f742d048d9405124
Md yasen rafe <mdyasenrafe@gmail.com>
2025-01-06 13:55:10 +0600

    Feat: Simplified SectionHeader for AddBudgetPage with core budget features

--- src/app/(main)/(dashboard)/budget-tracker/add-budget/page/AddBudgetPage.tsx
+++ src/app/(main)/(dashboard)/budget-tracker/add-budget/page/AddBudgetPage.tsx
@@ -1,10 +1,16 @@
 import { Text } from "@/components/atoms";
+import { SectionHeader } from "@/components/molecules";
 import React from "react";
 
 export const AddBudgetPage = () => {
   return (
-    <div>
-      <Text>This is a Add Budget Page</Text>
+    <div className="pt-10 pb-32 lg:pb-10">
+      <SectionHeader
+        title="Add Your Budget"
+        description={
+          "Set up your monthly spending limits and organize your expenses into categories. Keep track of your spending and carry over any leftover money to the next month. Start managing your money easily and effectively."
+        }
+      />
     </div>
   );
 };
