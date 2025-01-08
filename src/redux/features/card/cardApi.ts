5c9669b118d56effac177587beeb02cb05581846
Md yasen rafe <mdyasenrafe@gmail.com>
2025-01-09 03:21:37 +0600

    card api added

--- src/redux/features/card/cardApi.ts
+++ src/redux/features/card/cardApi.ts
@@ -1,13 +1,14 @@
 import { baseApi } from "@/api/baseApi";
 import { TCard } from "../cardOverview";
 import { TResponse } from "../types";
+import { TCreateCardPayload } from ".";
 
 const cardApi = baseApi.injectEndpoints({
   endpoints: (build) => ({
     getCards: build.query<TResponse<TCard[]>, void>({
       query: () => `/card`,
     }),
-    createCard: build.mutation<TResponse<TCard>, TCard>({
+    createCard: build.mutation<TResponse<TCard>, TCreateCardPayload>({
       query: (payload) => ({
         url: "/card",
         method: "POST",
@@ -17,4 +18,4 @@ const cardApi = baseApi.injectEndpoints({
   }),
 });
 
-export const { useGetCardsQuery } = cardApi;
+export const { useGetCardsQuery, useCreateCardMutation } = cardApi;
