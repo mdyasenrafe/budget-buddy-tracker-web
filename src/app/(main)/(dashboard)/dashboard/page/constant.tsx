3c58b02b3a4475e989711f293494a23265e6a73c
Md yasen rafe <mdyasenrafe@gmail.com>
2025-01-18 14:48:06 +0600

    updated metric color

--- src/app/(main)/(dashboard)/dashboard/page/constant.tsx
+++ src/app/(main)/(dashboard)/dashboard/page/constant.tsx
@@ -1,3 +1,4 @@
+import { colors } from "@/theme";
 import { BiWalletAlt } from "react-icons/bi";
 import { FiCreditCard } from "react-icons/fi";
 import { RiMoneyDollarCircleLine } from "react-icons/ri";
@@ -8,28 +9,28 @@ export const dashboardMetrics = [
     title: "Total Balance",
     value: "$12,345",
     icon: <BiWalletAlt size={24} />,
-    bgColor: "bg-[#D6E4FF]",
-    iconColor: "text-[#2F54EB]",
+    bgColor: colors.blue100,
+    iconColor: colors.blue500,
   },
   {
     title: "Monthly Expenses",
     value: "$3,210",
     icon: <RiMoneyDollarCircleLine size={24} />,
-    bgColor: "bg-[#FFE7BA]",
-    iconColor: "text-[#FA8C16]",
+    bgColor: colors.yellow100,
+    iconColor: colors.yellow500,
   },
   {
     title: "Remaining Budget",
     value: "$1,245",
     icon: <TbChartBar size={24} />,
-    bgColor: "bg-[#E6FFFB]",
-    iconColor: "text-[#13C2C2]",
+    bgColor: colors.green100,
+    iconColor: colors.green500,
   },
   {
     title: "Total Cards",
     value: "3 Cards",
     icon: <FiCreditCard size={24} />,
-    bgColor: "bg-[#F3E5F5]",
-    iconColor: "text-[#9C27B0]",
+    bgColor: colors.purple100,
+    iconColor: colors.purple500,
   },
 ];
