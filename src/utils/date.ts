b2d32e302ff41acba9a7247bab739ebda7164cbe
Md yasen rafe <mdyasenrafe@gmail.com>
2025-02-03 11:45:11 +0600

    added Dashboard Transactions month based filtering

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -1,5 +1,18 @@
 import dayjs from "dayjs";
+import utc from "dayjs/plugin/utc";
+import timezone from "dayjs/plugin/timezone";
+
+dayjs.extend(utc);
+dayjs.extend(timezone);
 
 export const TIMEZONE = Intl.DateTimeFormat().resolvedOptions().timeZone;
 export const CURRENTMONTHINDEX = dayjs().month();
 export const CURRENTYEAR = dayjs().year();
+
+export const getMonthStart = (timezone: string = "UTC"): string => {
+  return dayjs().tz(timezone).startOf("month").toISOString();
+};
+
+export const getMonthEnd = (timezone: string = "UTC"): string => {
+  return dayjs().endOf("month").tz(timezone).toISOString();
+};
